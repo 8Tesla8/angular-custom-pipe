8fb4087a6886fede076b7bc47737e592fa988ba1
tesla <kela256@gmail.com>
2019-08-25 19:21:58 +0300

    name

--- src/app/pipe/phone.pipe.ts
+++ src/app/pipe/phone.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: "phoneNumber"
+  name: "phone"
 })
 export class PhonePipe implements PipeTransform {
   transform(value: string): string {
